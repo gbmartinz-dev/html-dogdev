850a7e3663f8cc66e24339df6064e31a3f794665
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-05-29 21:08:23 -0300

    Finalizando a seção de depoimentos com o javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,46 @@ menuOpener.addEventListener('click', function() {
         menuOpener.querySelector('.hamburger-icon').style.display = 'none';
     }
 });
+
+// TESTIMONIALS
+
+let testimonials = [
+    { quote: '"Mais do que nunca, os animais de estimação são tratados como membros da família."', origin: 'cbs.svg' },
+    { quote: '"DogDev é um serviço de entrega direto ao consumidor, preparado na hora com ingredientes 100% reais. Ingredientes que os humanos reconheceriam."', origin: 'forbes.svg' },
+    { quote: '"DogDev usa ingredientes simples e limpos em seus pratos."', origin: 'fox.svg' },
+    { quote: '"Vejo você [Gabriel] como um verdadeiro herói"', origin: 'sharktank.svg' },
+];
+
+let testimonialQuote = document.querySelector('.testimonials .quote');
+let testimonialIcons = document.querySelector('.testimonials .icons');
+
+for(let tindex in testimonials) {
+    let img = document.createElement('img');
+    img.setAttribute('src', './assets/images/' + testimonials[parseInt(tindex)].origin);
+    img.style.cursor = 'pointer';
+    img.addEventListener('click', () => fillTestimonial(parseInt(tindex)));
+    testimonialIcons.appendChild(img);
+}
+
+let currentTestimonial = 0;
+let testimonialTimer;
+
+const fillTestimonial = (index) => {
+    clearTimeout(testimonialTimer);
+    currentTestimonial = index;
+    testimonialQuote.innerHTML = testimonials[currentTestimonial].quote;
+    let imgs = testimonialIcons.querySelectorAll('img');
+    for(let img of imgs) img.style.opacity = 0.2;
+    imgs[currentTestimonial].style.opacity = 1;
+    testimonialTimer = setTimeout(nextTestimonial, 3000);
+}
+
+const nextTestimonial = () => {
+    if (testimonials[currentTestimonial + 1]) {
+        fillTestimonial(currentTestimonial + 1);
+    } else {
+        fillTestimonial(0);
+    }
+}
+
+nextTestimonial();
